ad2c628ae1465174dbbfa87136ffe8432039b25d
soonfy <soonfy@163.com>
2016-11-14 18:02:06 +0800

    model

--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
 // require('./lib/index.js');
 // require('./lib/connection.js');
 // require('./lib/schemaType.js');
-require('./lib/schema.js');
+// require('./lib/schema.js');
+require('./lib/model.js');
+
 // require('./controller.js');
